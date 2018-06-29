8d02ceacd7495d997fc57016ea024ae85f52ed1e
Benny van Reeven <benny.van.reeven@mendix.com>
2018-06-29 15:03:39 +0200

    Add routing-controllers ZIP error endpoint

--- ZipController.ts
+++ ZipController.ts
@@ -11,7 +11,7 @@ export class ZipController {
     public getZip100MB(@Req() req: Request, @Res() res: Response) {
         const archive = archiver("zip");
 
-        // TODO: archive.on("error", next);
+        archive.on("error", (err) => req.destroy(err));
 
         archive.pipe(res);
 
@@ -20,6 +20,33 @@ export class ZipController {
 
         write100MB(passThrough, () => {
             passThrough.end(() => console.log("Done writing data!"));
+        });
+
+        archive.finalize();
+
+        return res;
+    }
+
+    @Get("/rc-zip100mberror")
+    public getZip100MBError(@Req() req: Request, @Res() res: Response) {
+        const archive = archiver("zip");
+
+        archive.on("error", (err) => {
+            console.log("Error in ZIP stream:", err.message);
+            // archive.abort();
+            //req.abort();
+            //res.connection.destroy(err);
+            //res.end();
+            req.destroy(err);
+        });
+
+        archive.pipe(res);
+
+        const passThrough = new PassThrough();
+        archive.append(passThrough, { name: "some-file.dat" });
+
+        write100MB(passThrough, () => {
+            passThrough.emit("error", new Error("OH NOES! ERROR IN SOME FILE STREAM!"));
         });
 
         archive.finalize();
